96ae229cae5c3cab9d10859322a365037ffb3a86
Mikhail Voevodin <72670840+MihailVoevodin@users.noreply.github.com>
2023-02-21 23:01:19 +0300

    add favicon and more info for movie page

--- components/Movie/MovieAbout/MovieAbout.tsx
+++ components/Movie/MovieAbout/MovieAbout.tsx
@@ -9,57 +9,69 @@ const MovieAbout = ({movie, movieStaff, movieBox}: any) => {
             <h3>О фильме</h3>
             <div className={styles.aboutItem}>
                 <div className={styles.aboutItemText}>Год производства</div>
-                <div className={styles.aboutItemContent}>{movie.year}</div>
+                <div>{movie.year}</div>
             </div>
             <div className={styles.aboutItem}>
                 <div className={styles.aboutItemText}>Страна</div>
-                <div className={styles.aboutItemContent}>
+                <div>
                     {movie.countries.map((c: any, id: number) => <span key={id}>{c.country}{id !== movie.countries.length - 1 ? ', ' : ''}</span>)}
                 </div>
             </div>
             <div className={styles.aboutItem}>
                 <div className={styles.aboutItemText}>Жанр</div>
-                <div className={styles.aboutItemContent}>
+                <div>
                     {movie.genres.map((c: any, id: number) => <span key={id}>{c.genre}{id !== movie.genres.length - 1 ? ', ' : ''}</span>)}
                 </div>
             </div>
             <div className={styles.aboutItem}>
                 <div className={styles.aboutItemText}>Слоган</div>
-                <div className={styles.aboutItemContent}>&#171;{movie.slogan}&#187;</div>
+                <div>{movie.slogan ? `&#171;${movie.slogan}&#187;` : '-'}</div>
             </div>
             <div className={styles.aboutItem}>
                 <div className={styles.aboutItemText}>Режиссер</div>
-                <div className={styles.aboutItemContent}><MovieAboutStaff array={movieStaff} profession={'DIRECTOR'}/></div>
+                <div><MovieAboutStaff array={movieStaff} profession={'DIRECTOR'}/></div>
             </div>
             <div className={styles.aboutItem}>
                 <div className={styles.aboutItemText}>Сценарий</div>
-                <div className={styles.aboutItemContent}><MovieAboutStaff array={movieStaff} profession={'WRITER'} /></div>
+                <div><MovieAboutStaff array={movieStaff} profession={'WRITER'} /></div>
             </div>
             <div className={styles.aboutItem}>
                 <div className={styles.aboutItemText}>Продюсер</div>
-                <div className={styles.aboutItemContent}><MovieAboutStaff array={movieStaff} profession={'PRODUCER'} /></div>
+                <div><MovieAboutStaff array={movieStaff} profession={'PRODUCER'} /></div>
             </div>
             <div className={styles.aboutItem}>
                 <div className={styles.aboutItemText}>Оператор</div>
-                <div className={styles.aboutItemContent}><MovieAboutStaff array={movieStaff} profession={'OPERATOR'} /></div>
+                <div><MovieAboutStaff array={movieStaff} profession={'OPERATOR'} /></div>
             </div>
             <div className={styles.aboutItem}>
                 <div className={styles.aboutItemText}>Композитор</div>
-                <div className={styles.aboutItemContent}><MovieAboutStaff array={movieStaff} profession={'COMPOSER'} /></div>
+                <div><MovieAboutStaff array={movieStaff} profession={'COMPOSER'} /></div>
             </div>
             <div className={styles.aboutItem}>
                 <div className={styles.aboutItemText}>Художник</div>
-                <div className={styles.aboutItemContent}><MovieAboutStaff array={movieStaff} profession={'DESIGN'} /></div>
+                <div><MovieAboutStaff array={movieStaff} profession={'DESIGN'} /></div>
             </div>
             <div className={styles.aboutItem}>
                 <div className={styles.aboutItemText}>Монтаж</div>
-                <div className={styles.aboutItemContent}><MovieAboutStaff array={movieStaff} profession={'EDITOR'} /></div>
+                <div><MovieAboutStaff array={movieStaff} profession={'EDITOR'} /></div>
             </div>
             <MovieAboutBox array={movieBox.items} boxType={'BUDGET'} boxText={'Бюджет'} />
             <MovieAboutBox array={movieBox.items} boxType={'MARKETING'} boxText={'Маркетинг'} />
             <MovieAboutBox array={movieBox.items} boxType={'USA'} boxText={'Сборы в США'} />
             <MovieAboutBox array={movieBox.items} boxType={'RUS'} boxText={'Сборы в России'} />
             <MovieAboutBox array={movieBox.items} boxType={'WORLD'} boxText={'Сборы в мире'} />
+            <div className={styles.aboutItem}>
+                <div className={styles.aboutItemText}>Возраст</div>
+                <div className={styles.aboutItemBorder}>{movie.ratingAgeLimits.slice(3, 5)}+</div>
+            </div>
+            {movie.ratingMpaa && <div className={styles.aboutItem}>
+                <div className={styles.aboutItemText}>Рейтинг MPAA</div>
+                <div className={styles.aboutItemBorder}>{movie.ratingMpaa.toUpperCase()}</div>
+            </div>}
+            <div className={styles.aboutItem}>
+                <div className={styles.aboutItemText}>Время</div>
+                <div>{movie.filmLength} мин. / {Math.floor(movie.filmLength / 60)}:{movie.filmLength % 60}</div>
+            </div>
         </div>
     )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,13 @@ import Layout from 'components/Layout/Layout';
 import type { AppProps } from "next/app";
 import {Provider} from 'react-redux';
 import {store} from 'store/Store';
+import NextNProgress from 'nextjs-progressbar';
 
 const App = ({ Component, pageProps }: AppProps) => {
    return (
        <Provider store={store}>
            <Layout>
+               <NextNProgress color="#ff6200" startPosition={0.3} stopDelayMs={200} height={3} showOnShallow={true} />
                <Component {...pageProps} />
            </Layout>
        </Provider>
