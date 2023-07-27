728e9630728366b75aa4181e6d175dd2ecdfcb6e
Mikhail Voevodin <72670840+MihailVoevodin@users.noreply.github.com>
2023-07-27 23:18:37 +0300

    add active class for tops

--- components/Navbar/TopsNavbar.tsx
+++ components/Navbar/TopsNavbar.tsx
@@ -1,5 +1,6 @@
 import styles from 'components/Navbar/TopsNavbar.module.scss';
 import Link from 'next/link';
+import {useRouter} from 'next/router';
 import {FC} from 'react';
 import {T} from 'Common/Text';
 
@@ -17,12 +18,34 @@ interface IProps {
 }
 
 export const TopsNavbar: FC<IProps> = ({top250PageId, top100PageId, topAwaitPageId, findMoviesPageId}) => {
+    const router = useRouter();
+
     return (
         <div className={styles.topsNavbarContainer}>
-            <Link href={T.Pages.MainPages.Top250Movies.link(top250PageId)}>{T.Pages.MainPages.Top250Movies.text}</Link>
-            <Link href={T.Pages.MainPages.Top100Movies.link(top100PageId)}>{T.Pages.MainPages.Top100Movies.text}</Link>
-            <Link href={T.Pages.MainPages.TopAwaitMovies.link(topAwaitPageId)}>{T.Pages.MainPages.TopAwaitMovies.text}</Link>
-            <Link href={T.Pages.MainPages.FindMovies.link(findMoviesPageId)}>{T.Pages.MainPages.FindMovies.text}</Link>
+            <Link
+                className={router.pathname.includes('top250movies') ? styles.active : undefined}
+                href={T.Pages.MainPages.Top250Movies.link(top250PageId)}
+            >
+                {T.Pages.MainPages.Top250Movies.text}
+            </Link>
+            <Link
+                className={router.pathname.includes('top100popular') ? styles.active : undefined}
+                href={T.Pages.MainPages.Top100Movies.link(top100PageId)}
+            >
+                {T.Pages.MainPages.Top100Movies.text}
+            </Link>
+            <Link
+                className={router.pathname.includes('topAwaitMovies') ? styles.active : undefined}
+                href={T.Pages.MainPages.TopAwaitMovies.link(topAwaitPageId)}
+            >
+                {T.Pages.MainPages.TopAwaitMovies.text}
+            </Link>
+            <Link
+                className={router.pathname.includes('findMovies') ? styles.active : undefined}
+                href={T.Pages.MainPages.FindMovies.link(findMoviesPageId)}
+            >
+                {T.Pages.MainPages.FindMovies.text}
+            </Link>
         </div>
     );
 };
