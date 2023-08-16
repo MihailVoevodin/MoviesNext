bf62fa7f9fc16fbc3980db0c25174141fad8b38e
Mikhail Voevodin <72670840+MihailVoevodin@users.noreply.github.com>
2023-08-16 22:07:28 +0300

    change font family, format scss files to similar code style, fix some styles

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,11 +7,9 @@ export default function Document() {
     return (
         <Html>
             <Head>
-                <link
-                    href="//db.onlinewebfonts.com/c/a1750a4dae8db954fdd595f313b3b34e?family=Graphik+Kinopoisk+LC+Web+SB"
-                    rel="stylesheet"
-                    type="text/css"
-                />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Source+Sans+3:wght@400;700&display=swap" rel="stylesheet" />
             </Head>
             <body>
                 <Main />
