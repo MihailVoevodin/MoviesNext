29aa430d32dcf33fe9dc61b8ae894a8be7d71884
Mikhail Voevodin <72670840+MihailVoevodin@users.noreply.github.com>
2023-10-05 23:02:53 +0300

    add profile logic and markup

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,17 +1,33 @@
 import {HeadComponent} from 'components/Head/Head';
-import {useSession} from 'next-auth/react';
+import {signIn, signOut, useSession} from 'next-auth/react';
+import Image from 'next/image';
 
-const Signin = () => {
+const Profile = () => {
     const {data: session} = useSession();
 
     return (
         <>
-            <HeadComponent title={`Профиль ${session?.user?.name}`} />
+            <HeadComponent title={`Профиль ${session ? session.user?.name : ''}`} />
             <main>
-                <div>{session?.user?.name}</div>
+                <div>
+                    {!session && (
+                        <>
+                            <div>Вы не авторизованы</div>
+                            <div onClick={() => signIn()}>Войти</div>
+                        </>
+                    )}
+                    {session && (
+                        <>
+                            <Image src={`${session.user?.image}`} alt={''} width={100} height={100} />
+                            <div>Имя: {session.user?.name}</div>
+                            <div>Почта: {session.user?.email}</div>
+                            <div onClick={() => signOut()}>Выйти</div>
+                        </>
+                    )}
+                </div>
             </main>
         </>
     );
 };
 
-export default Signin;
+export default Profile;
