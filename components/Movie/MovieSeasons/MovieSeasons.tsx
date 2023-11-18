c21065d6c0ce04e78cc8a09acdce351dbea14e2a
Mikhail Voevodin <72670840+MihailVoevodin@users.noreply.github.com>
2023-11-19 00:42:48 +0300

    start markup for seasons

--- components/Movie/MovieSeasons/MovieSeasons.tsx
+++ components/Movie/MovieSeasons/MovieSeasons.tsx
@@ -1,16 +1,25 @@
+import styles from 'components/Movie/MovieSeasons/MovieSeasons.module.scss';
 import {FC} from 'react';
 import {ISeason} from 'Common/Models';
+import {T} from 'Common/Text';
 
 interface IProps {
     movieSeasons: ISeason[];
 }
 
 export const MovieSeasons: FC<IProps> = ({movieSeasons}) => {
+    console.log(movieSeasons);
+
     return (
         <>
-            {movieSeasons.map((season) => (
-                <div key={season.number}>{season.number}</div>
-            ))}
+            <div className={styles.seasonNumberContainer}>
+                <div>{T.Pages.Seasons.label}:</div>
+                {movieSeasons.map((season) => (
+                    <div key={season.number} className={styles.seasonNumber}>
+                        {season.number}
+                    </div>
+                ))}
+            </div>
         </>
     );
 };
